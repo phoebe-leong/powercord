dde47236f1f1db4f28ed3f2ca93502f43137bc3f
ph3b3 <phoebeleong8@gmail.com>
2023-04-18 12:01:38 +1000

    added else block to if statement

--- injectors/darwin.js
+++ injectors/darwin.js
@@ -5,13 +5,13 @@ read userPath
 
 if [userPath != ""]:
   exports.getAppDir = userPath
+else:
+  const PATHS = {
+    stable: '/Applications/Discord.app/Contents/Resources/app',
+    ptb: '/Applications/Discord PTB.app/Contents/Resources/app',
+    canary: '/Applications/Discord Canary.app/Contents/Resources/app',
+    dev: '/Applications/Discord Development.app/Contents/Resources/app'
+  };
+
+  exports.getAppDir = async (platform) => PATHS[platform];
 fi
-
-const PATHS = {
-  stable: '/Applications/Discord.app/Contents/Resources/app',
-  ptb: '/Applications/Discord PTB.app/Contents/Resources/app',
-  canary: '/Applications/Discord Canary.app/Contents/Resources/app',
-  dev: '/Applications/Discord Development.app/Contents/Resources/app'
-};
-
-exports.getAppDir = async (platform) => PATHS[platform];
